b7a804336df2c6e89efba850fc47c7456e374441
diego <sandoval.vargas.diego.1c@gmail.com>
2022-12-18 22:31:12 -0600

    Fusion Menu version Mobile

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+
+//Desktop Menu
 const desktopMenu = document.querySelector('.desktop-menu')
 const navbarEmail = document.querySelector('.navbar-email')
 
 navbarEmail.addEventListener('click', () => {
   desktopMenu.classList.toggle('inactive')
 })
+
+//Mobile Menu
+const mobileMenu = document.querySelector('.mobile-menu')
+const imgMenu = document.querySelector('.menu')
+
+imgMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('inactive');
+  console.log('click')
+})
+
