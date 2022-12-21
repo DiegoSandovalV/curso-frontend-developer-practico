46b6ba4fca76d08c27752f9cf5ed8c2c75943562
diego <sandoval.vargas.diego.1c@gmail.com>
2022-12-20 23:07:42 -0600

    Creation of products using JavaScript

--- app.js
+++ app.js
@@ -1,18 +1,107 @@
 
 //Desktop Menu
-const desktopMenu = document.querySelector('.desktop-menu')
-const navbarEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu');
+const navbarEmail = document.querySelector('.navbar-email');
 
 navbarEmail.addEventListener('click', () => {
-  desktopMenu.classList.toggle('inactive')
-})
+
+  if(shoppingCar.classList.contains('inactive') == false) {
+    shoppingCar.classList.add('inactive');
+  }
+  desktopMenu.classList.toggle('inactive');
+});
 
 //Mobile Menu
-const mobileMenu = document.querySelector('.mobile-menu')
-const imgMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const imgMenu = document.querySelector('.menu');
 
 imgMenu.addEventListener('click', () => {
+  if(shoppingCar.classList.contains('inactive') == false){
+    shoppingCar.classList.add('inactive');
+  }
   mobileMenu.classList.toggle('inactive');
-  console.log('click')
-})
+});
 
+//shopping car
+const shoppingCarImg = document.querySelector('.navbar-shopping-cart');
+const shoppingCar = document.querySelector('.product-detail');
+
+shoppingCarImg.addEventListener('click', () => {
+  if(desktopMenu.classList.contains('inactive') == false){
+    desktopMenu.classList.add('inactive');
+  }else if(mobileMenu.classList.contains('inactive') == false){
+    mobileMenu.classList.add('inactive');
+  }
+  shoppingCar.classList.toggle('inactive');
+});
+
+//Products
+
+const cardsContainer = document.querySelector('.cards-container')
+
+const productList = [];
+
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+  name: 'Camera',
+  price: 150,
+  image: 'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productList.push({
+  name: 'Clock',
+  price: 80,
+  image: 'https://images.pexels.com/photos/5412086/pexels-photo-5412086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productList.push({
+  name: 'Rubik Cube',
+  price: 100,
+  image: 'https://images.pexels.com/photos/19677/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+//Creation of products
+
+ function renderProducts(productList){
+  for(product of productList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productinfo = document.createElement('div');
+    productinfo.classList.add('product-info');
+  
+    const productChar = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+  
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productChar.append(productPrice,productName);
+  
+    const productInfoFigure = document.createElement('figure');
+   
+  
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+  
+    productInfoFigure.appendChild(productImg)
+  
+    productinfo.append(productChar,productInfoFigure);
+  
+    productCard.append(productImg, productinfo);
+  
+    cardsContainer.appendChild(productCard);
+   }
+ }
+
+renderProducts(productList);
